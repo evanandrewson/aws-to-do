153b226806a38355de0df9f3da72ff122f90e94b
evanandrewson <evanandrewson@gmail.com>
2020-08-17 09:54:39 -0700

    set up repo, created form component

---
+++ src/Components/form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Form = () => {
+
+    const handleSubmit = () => {};
+    
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="todo"></input>
+            <button></button>
+        </form>
+    )
+};
+
+export default Form;
